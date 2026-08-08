16a54cb2710ba9bcdc308619754742018b1c903f
Faizan Faisal <faizanf114@gmail.com>
2026-08-08 11:26:44 -0700

    font fix

--- dev/tailwind.config.js
+++ dev/tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        sans: ['"STIX Two Text"', ...defaultTheme.fontFamily.serif],
       },
       colors: {
         brand: {
